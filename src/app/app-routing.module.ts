1cb8f1b7c1669e92a6e0c274068b38252f92985e
Moritz Broda <mo.brodaqgmx.de>
2021-08-16 20:47:31 +0200

    easter egg hermes added and beautified contact component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './feature/contact/contact.component';
+import { HermesComponent } from './feature/easter-eggs/hermes/hermes.component';
 import { HomeComponent } from './feature/home/home.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'contact', component: ContactComponent},
+  {path: 'hermes', component: HermesComponent},
 ];
 
 @NgModule({
@@ -14,4 +16,4 @@ const routes: Routes = [
 })
 export class AppRoutingModule { }
 
-export const routingComponents = [HomeComponent, ContactComponent]
+export const routingComponents = [HomeComponent, ContactComponent,HermesComponent]
---
+++ src/app/feature/easter-eggs/hermes/hermes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hermes',
+  templateUrl: './hermes.component.html',
+  styleUrls: ['./hermes.component.scss']
+})
+export class HermesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
